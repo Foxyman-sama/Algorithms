8a8915e9aad3a44396343c7e5d89e6e284212e3e
Foxyman-sama <dimalatoho1448@gmail.com>
2024-09-30 11:29:50 +0300

    feat(dynamic_array): add functions for array

--- ds/dynamic_array.h
+++ ds/dynamic_array.h
@@ -3,15 +3,82 @@
 
 #include <stdlib.h>
 
+#include <cstdlib>
+
 struct DynamicArray {
   int* ptr;
   int size;
   int capacity;
 };
 
-struct DynamicArray create_array(int capacity) {
+inline struct DynamicArray create_array(int capacity) {
+  if (capacity < 1) {
+    abort();
+  }
+
   struct DynamicArray result = { (int*)calloc(capacity, sizeof(int)), 0, capacity };
   return result;
+}
+
+inline void clear_array(struct DynamicArray* array) {
+  free(array->ptr);
+  array->ptr = NULL;
+  array->size = 0;
+  array->capacity = 0;
+}
+
+inline void insert_into_array(struct DynamicArray* array, int index, int elem) {
+  if (index < 0 || index > array->size) {
+    abort();
+  }
+
+  if (array->capacity <= array->size) {
+    array->capacity *= 2;
+    array->ptr = (int*)realloc(array->ptr, array->capacity);
+  }
+
+  if (index != array->size) {
+    int* temp = (int*)calloc(array->size - index, sizeof(array->ptr[0]));
+    for (int i = index; i < array->size; ++i) {
+      temp[i - index] = array->ptr[i];
+    }
+
+    array->ptr[index] = elem;
+    for (int i = index + 1; i < array->size + 1; ++i) {
+      array->ptr[i] = temp[i - index - 1];
+    }
+  } else {
+    array->ptr[index] = elem;
+  }
+
+  ++array->size;
+}
+
+inline void remove_from_array(struct DynamicArray* array, int index) {
+  if (index < 0 || index >= array->size) {
+    abort();
+  }
+
+  if (index != array->size) {
+    int* temp = (int*)calloc(array->size - index - 1, sizeof(array->ptr[0]));
+    for (int i = index + 1; i < array->size; ++i) {
+      temp[i - index - 1] = array->ptr[i];
+    }
+
+    for (int i = index; i < array->size - 1; ++i) {
+      array->ptr[i] = temp[i - index];
+    }
+  }
+
+  --array->size;
+}
+
+inline int get(struct DynamicArray* array, int index) {
+  if (index < 0 || index >= array->size) {
+    abort();
+  }
+
+  return array->ptr[index];
 }
 
 #endif
---
+++ tests/dynamic_array/dynamic_array.cc
@@ -0,0 +1,81 @@
+#include "ds/dynamic_array.h"
+
+#include <gtest/gtest.h>
+
+#include "gtest/gtest.h"
+
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
+
+class dynamic_array_tests : public testing::Test {};
+
+TEST_F(dynamic_array_tests, create_and_clear_dynamic_array) {
+  struct DynamicArray array = create_array(10);
+  clear_array(&array);
+
+  ASSERT_EQ(array.ptr, nullptr);
+  ASSERT_EQ(array.size, 0);
+  ASSERT_EQ(array.capacity, 0);
+}
+
+TEST_F(dynamic_array_tests, create_dynamic_array_using_bad_index) {
+  ASSERT_DEATH({ struct DynamicArray array = create_array(0); }, "");
+}
+
+TEST_F(dynamic_array_tests, insert_into_dynamic_array) {
+  struct DynamicArray array = create_array(1);
+
+  insert_into_array(&array, 0, 321);
+  insert_into_array(&array, 1, 5);
+  insert_into_array(&array, 2, 7);
+  insert_into_array(&array, 2, 23);
+
+  ASSERT_EQ(array.ptr[0], 321);
+  ASSERT_EQ(array.ptr[1], 5);
+  ASSERT_EQ(array.ptr[2], 23);
+}
+
+TEST_F(dynamic_array_tests, insert_into_dynamic_array_using_bad_index) {
+  struct DynamicArray array = create_array(1);
+
+  insert_into_array(&array, 0, 321);
+  ASSERT_DEATH(insert_into_array(&array, 2, 5), "");
+}
+
+TEST_F(dynamic_array_tests, remove_from_dynamic_array) {
+  struct DynamicArray array = create_array(1);
+
+  insert_into_array(&array, 0, 321);
+  insert_into_array(&array, 1, 5);
+  insert_into_array(&array, 2, 7);
+
+  remove_from_array(&array, 1);
+
+  ASSERT_EQ(array.ptr[0], 321);
+  ASSERT_EQ(array.ptr[1], 7);
+  ASSERT_EQ(array.size, 2);
+}
+
+TEST_F(dynamic_array_tests, remove_from_dynamic_array_using_bad_index) {
+  struct DynamicArray array = create_array(1);
+
+  insert_into_array(&array, 0, 321);
+
+  ASSERT_DEATH(remove_from_array(&array, 1), "");
+}
+
+TEST_F(dynamic_array_tests, get_element_from_dynamic_array) {
+  struct DynamicArray array = create_array(1);
+  insert_into_array(&array, 0, 321);
+
+  ASSERT_EQ(321, get(&array, 0));
+}
+
+TEST_F(dynamic_array_tests, get_element_from_dynamic_array_using_bad_index) {
+  struct DynamicArray array = create_array(1);
+  insert_into_array(&array, 0, 321);
+
+  ASSERT_DEATH(get(&array, 1), "");
+}
