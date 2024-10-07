962e2b2934274f860d1e659b8695c4b56578c6e8
Foxyman-sama <dimalatoho1448@gmail.com>
2024-10-07 11:30:01 +0300

    refactoring(dynamic_array): refactor insert function

--- ds/dynamic_array.cc
+++ ds/dynamic_array.cc
@@ -15,33 +15,55 @@ void clear_array(DynamicArray* array) {
   array->capacity = 0;
 }
 
+void increase_capacity_twice(DynamicArray* array);
+void make_empty_field_for_inserting(DynamicArray* array, int index);
+int* copy_range(DynamicArray* array, int begin, int end);
+void insert_element(DynamicArray* array, int index, int elem);
+
 bool insert_into_array(DynamicArray* array, int index, int elem) {
   if (index < 0 || index > array->size) {
     return false;
   }
 
   if (array->capacity <= array->size) {
-    array->capacity *= 2;
-    array->ptr = (int*)realloc(array->ptr, array->capacity);
+    increase_capacity_twice(array);
   }
 
   if (index != array->size) {
-    int* temp = (int*)calloc(array->size - index, sizeof(array->ptr[0]));
-    for (int i = index; i < array->size; ++i) {
-      temp[i - index] = array->ptr[i];
-    }
-
-    array->ptr[index] = elem;
-    for (int i = index + 1; i < array->size + 1; ++i) {
-      array->ptr[i] = temp[i - index - 1];
-    }
-  } else {
-    array->ptr[index] = elem;
+    make_empty_field_for_inserting(array, index);
   }
 
-  ++array->size;
+  insert_element(array, index, elem);
 
   return true;
+}
+
+void increase_capacity_twice(DynamicArray* array) {
+  array->capacity *= 2;
+  array->ptr = (int*)realloc(array->ptr, array->capacity);
+}
+
+void make_empty_field_for_inserting(DynamicArray* array, int index) {
+  int* temp = copy_range(array, index, array->size);
+
+  for (int i = index + 1; i < array->size + 1; ++i) {
+    array->ptr[i] = temp[i - index - 1];
+  }
+}
+
+int* copy_range(DynamicArray* array, int begin, int end) {
+  int* result = (int*)calloc(end - begin, sizeof(array->ptr[0]));
+
+  for (int i = begin; i < end; ++i) {
+    result[i - begin] = array->ptr[i];
+  }
+
+  return result;
+}
+
+void insert_element(DynamicArray* array, int index, int elem) {
+  array->ptr[index] = elem;
+  ++array->size;
 }
 
 bool remove_from_array(DynamicArray* array, int index) {
