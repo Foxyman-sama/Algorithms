8da70cc4e6ee0642ab9b29c5d361fbaca79ce512
Foxyman-sama <dimalatoho1448@gmail.com>
2024-10-07 11:16:20 +0300

    refactoring(binary_search): divide into header and cpp file

---
+++ algorithms/binary_search.cc
@@ -0,0 +1,22 @@
+#include "binary_search.h"
+
+int binary_search(DynamicArray* array, int l, int r, int found) {
+  if (l > r) {
+    return -1;
+  }
+
+  int m = (l + r) / 2;
+  int actual = get(array, m);
+  if (found == actual) {
+    return m;
+  }
+  if (l == r) {
+    return -1;
+  }
+
+  if (found < actual) {
+    return binary_search(array, l, m - 1, found);
+  } else {
+    return binary_search(array, m + 1, r, found);
+  }
+}
--- algorithms/binary_search.h
+++ algorithms/binary_search.h
@@ -1,27 +1,8 @@
 #ifndef BINARY_SEARCH_H
 #define BINARY_SEARCH_H
 
-#include "dynamic_array.h"
+#include "../ds/dynamic_array.h"
 
-int binary_search(DynamicArray* array, int l, int r, int found) {
-  if (l > r) {
-    return -1;
-  }
-
-  int m = (l + r) / 2;
-  int actual = get(array, m);
-  if (found == actual) {
-    return m;
-  }
-  if (l == r) {
-    return -1;
-  }
-
-  if (found < actual) {
-    return binary_search(array, l, m - 1, found);
-  } else {
-    return binary_search(array, m + 1, r, found);
-  }
-}
+int binary_search(DynamicArray* array, int l, int r, int found);
 
 #endif
