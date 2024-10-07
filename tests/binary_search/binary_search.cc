15c1eb6d0cd116ced9bfaaf37b13964eb1e41960
Foxyman-sama <dimalatoho1448@gmail.com>
2024-10-07 11:04:43 +0300

    feat(binary_search): realize binary search and tests

---
+++ tests/binary_search/binary_search.cc
@@ -0,0 +1,34 @@
+#include "algorithms/binary_search.h"
+
+#include <gtest/gtest.h>
+
+#include "ds/dynamic_array.h"
+#include "gtest/gtest.h"
+
+int main() {
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
+}
+
+class binary_search_tests : public testing::Test {};
+
+TEST_F(binary_search_tests, find_in_array_with_size_one) {
+  DynamicArray array = create_array(1);
+  insert_into_array(&array, 0, 321);
+
+  const int actual = binary_search(&array, 0, array.size - 1, 321);
+
+  ASSERT_EQ(0, actual);
+}
+
+TEST_F(binary_search_tests, find_in_array_with_size_four) {
+  DynamicArray array = create_array(1);
+  insert_into_array(&array, 0, 1);
+  insert_into_array(&array, 1, 23);
+  insert_into_array(&array, 2, 33);
+  insert_into_array(&array, 3, 444);
+
+  const int actual = binary_search(&array, 0, array.size - 1, 33);
+
+  ASSERT_EQ(2, actual);
+}
